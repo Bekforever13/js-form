71914fe772653e788894f6241e87c4f992d03c23
Bekforever13 <b3kforever@gmail.com>
2023-01-29 17:46:45 +0500

    simplified js

--- main.js
+++ main.js
@@ -1,27 +1,16 @@
-let firstName
-let lastName
-let date
-let born
-let favMovie
-let actor
-let singer
-let city
-let hobby1
-let hobby2
-let hobby3
+let firstName = document.getElementById('firstName').value
+let lastName = document.getElementById('lastName').value
+let date = document.getElementById('date').value
+let born = document.getElementById('born').value
+let favMovie = document.getElementById('favMovie').value
+let actor = document.getElementById('actor').value
+let singer = document.getElementById('singer').value
+let city = document.getElementById('city').value
+let hobby1 = document.getElementById('hobby1').value
+let hobby2 = document.getElementById('hobby2').value
+let hobby3 = document.getElementById('hobby3').value
 
 document.getElementById('myButton').onclick = function () {
-	firstName = document.getElementById('firstName').value
-	lastName = document.getElementById('lastName').value
-	date = document.getElementById('date').value
-	born = document.getElementById('born').value
-	favMovie = document.getElementById('favMovie').value
-	actor = document.getElementById('actor').value
-	singer = document.getElementById('singer').value
-	city = document.getElementById('city').value
-	hobby1 = document.getElementById('hobby1').value
-	hobby2 = document.getElementById('hobby2').value
-	hobby3 = document.getElementById('hobby3').value
 	document.write(
 		'Your first name is ' +
 			firstName +
